0ab85156fde24919f8ddefb4ecc840f522d4e727
Tanay <tanay15802@gmail.com>
2023-07-29 10:54:30 +0530

    NFTMarketplace using Alchemy tutorial

---
+++ src/components/Marketplace.js
@@ -0,0 +1,91 @@
+import Navbar from "./Navbar";
+import NFTTile from "./NFTTile";
+import MarketplaceJSON from "../Marketplace.json";
+import axios from "axios";
+import { useState } from "react";
+
+export default function Marketplace() {
+const sampleData = [
+    {
+        "name": "NFT#1",
+        "description": "Sample NFT 1",
+        "website":"http://axieinfinity.io",
+        "image":"https://gateway.pinata.cloud/ipfs/QmNaCTM1E39PrxhPCPPu2uAER6c52HyCwF6bouhVTdsdrF",
+        "price":"0.03ETH",
+        "currentlySelling":"True",
+        "address":"0xe81Bf5A757CB4f7F82a2F23b1e59bE45c33c5b13",
+    },
+    {
+        "name": "NFT#2",
+        "description": "Alchemy's Second NFT",
+        "website":"http://axieinfinity.io",
+        "image":"https://gateway.pinata.cloud/ipfs/QmUBxzx62XuqohC5M3scdMXAB5j55R1GjZF6BTCuiWo8qf",
+        "price":"0.03ETH",
+        "currentlySelling":"True",
+        "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
+    },
+    {
+        "name": "NFT#3",
+        "description": "Alchemy's Third NFT",
+        "website":"http://axieinfinity.io",
+        "image":"https://gateway.pinata.cloud/ipfs/QmaLa833cL9kgsNqRttt8YdKiyDgGAZCm6d1ADv6EwKepD",
+        "price":"0.03ETH",
+        "currentlySelling":"True",
+        "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
+    },
+];
+const [data, updateData] = useState(sampleData);
+const [dataFetched, updateFetched] = useState(false);
+
+async function getAllNFTs() {
+    const ethers = require("ethers");
+    
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    
+    let contract = new ethers.Contract(MarketplaceJSON.address, MarketplaceJSON.abi, signer)
+    //create an NFT Token
+    let transaction = await contract.getAllNFTs()
+
+    const items = await Promise.all(transaction.map(async i => {
+        const tokenURI = await contract.tokenURI(i.tokenId);
+        let meta = await axios.get(tokenURI);
+        meta = meta.data;
+
+        let price = ethers.utils.formatUnits(i.price.toString(), 'ether');
+        let item = {
+            price,
+            tokenId: i.tokenId.toNumber(),
+            seller: i.seller,
+            owner: i.owner,
+            image: meta.image,
+            name: meta.name,
+            description: meta.description,
+        }
+        return item;
+    }))
+
+    updateFetched(true);
+    updateData(items);
+}
+
+if(!dataFetched)
+    getAllNFTs();
+
+return (
+    <div>
+        <Navbar></Navbar>
+        <div className="flex flex-col place-items-center mt-20">
+            <div className="md:text-xl font-bold text-white">
+                Top NFTs
+            </div>
+            <div className="flex mt-5 justify-between flex-wrap max-w-screen-xl text-center">
+                {data.map((value, index) => {
+                    return <NFTTile data={value} key={index}></NFTTile>;
+                })}
+            </div>
+        </div>            
+    </div>
+);
+
+}
